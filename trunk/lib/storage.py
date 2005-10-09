84a8fc697912fac0ab1dbc9efdc679054005762f
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2005-10-09 19:15:13 +0000

    Work on storage continues.

--- trunk/lib/storage.py
+++ trunk/lib/storage.py
@@ -20,7 +20,9 @@
 
 """Data storage."""
 
+import time
 import os, os.path as op
+import cPickle as pickle
 import metakit
 
 __revision__ = '$Revision$'
@@ -30,27 +32,53 @@ class Storage:
 
     def __init__(self):
         fileName = op.expanduser('~/.jpa2/entries')
-        if not op.isfile(fileName):
+        if op.isfile(fileName):
+            self.storage = metakit.storage(fileName, 1)
+            self.entries = self.storage.view('entries')
+        else:
             self.initStorage(fileName)
-        self.storage = metakit.storage(fileName, 1)
-        self.entries = self.storage.getas('entries[eid:S,created:S,sent:S,'
-            'title:S,body:S,year:I,month:I]')
 
     def initStorage(self, fileName):
         dirName = op.dirname(fileName)
         if not op.isdir(dirName):
             os.makedirs(dirName)
+        self.storage = metakit.storage(fileName, 1)
+        self.entries = self.storage.getas('entries[eid:S,created:I,sent:S,'
+            'edited:S,title:S,body:S,year:I,month:I]')
     
-    def getEntriesList(self, year, month):
-        view = self.entries.select({'year': year}, {'month': month})
-        entries = view.project(metakit.property('S', 'eid'))
-        ret = []
-        for e in entries:
-            ret.append(e.eid)
-        return ret
+    def getEntriesList(self, entryYear, entryMonth):
+        view = self.entries.select(year=entryYear, month=entryMonth)
+        return view.project(metakit.property('S', 'eid'))
     
-    def getEntries(self, year, month):
-        pass
+    def getEntries(self, entryYear, entryMonth):
+        return self.entries.select(year=entryYear, month=entryMonth)
+
+    def getEntry(self, entryId):
+        entry = {}
+        row = self.entries.find(eid=entryId)
+        if row:
+            entry['eid'] = row.eid
+            entry['created'] = row.created
+            entry['sent'] = pickle.loads(row.sent)
+            entry['edited'] = pickle.loads(row.edited)
+            entry['title'] = row.title
+            entry['body'] = row.body
+        return entry
+
+    def setEntry(self, entry, entryId=None):
+        if entryId:
+            row = self.entries.find(eid=entryId)
+            row.created = pickle.dumps(entry['created'], 
+                pickle.HIGHEST_PROTOCOL)
+            row.sent = pickle.dumps(entry['sent'], pickle.HIGHEST_PROTOCOL)
+            row.edited = pickle.dumps(entry['edited'], pickle.HIGHEST_PROTOCOL)
+            row.title = entry['title']
+            row.body = entry['body']
+        else:
+            entry['eid'] = time.strftime('%Y%m%d%H%M%S', entry['created'])
+            entry.year = entry['created'][0]
+            entry.month = entry['created'][1]
+            self.entries.append(**entry)
 
     def close(self):
         self.storage.commit()
