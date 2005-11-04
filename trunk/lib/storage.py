39444bf02a6fea10c1dd0288c62d143a919b7325
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2005-11-04 21:16:02 +0000

    Decided not to go with Metakit, stick to SQLite (1 and 2) and built-ins (bsddb and shelve).

--- trunk/lib/storage.py
+++ trunk/lib/storage.py
@@ -22,7 +22,6 @@ __revision__ = '$Id$'
 
 import time
 import os, os.path as op
-import cPickle as pickle
 
 
 def checkAvailableStorage():
@@ -31,33 +30,12 @@ def checkAvailableStorage():
     As this function tries to import any of interesting modules, it should't be
     run too often."""
     engines = []
-    try:
-        # try metakit
-        import metakit
-        engines.append('metakit')
-        del metakit
-    except ImportError:
-        # no metakit
-        pass
-    try:
-        # try new pysqlite (pysqlite2)
-        import pysqlite2
-        engines.append('sqlite2')
-        del pysqlite2
-    except ImportError:
-        # no mysqlite2
-        pass
-    try:
-        # try old pysqlite (pysqlite1)
-        import sqlite
-        engines.append('sqlite1')
-        del sqlite
-    except ImportError:
-        # no sqlite1
-        pass
-    # the next two should be always available on any contemporary Python
-    engines.append('bsddb')
-    engines.append('shelve')
+    for storage in ('pysqlite2', 'sqlite', 'bsddb', 'shelve'):
+        try:
+            __import__(storage)
+            engines.append(storage)
+        except ImportError:
+            pass
     return tuple(engines)
 
 
@@ -67,19 +45,19 @@ def getStorage(config):
     The default option is the lowest common denominator, the standard Python's
     shelve module."""
     storageType = config.getOption('internal', 'storage', 'shelve')
-    if storageType == 'metakit':
-        return MetakitStorage()
-    elif storageType == 'sqlite1':
+    if storageType == 'sqlite':
         return SQLite1Storage()
-    elif storageType == 'shelve':
+    elif storageType == 'pysqlite2':
+        return SQLite2Storage()
+    elif storageType == 'bsddb':
+        return DBStorage()
+    else:
         return ShelveStorage()
 
 
 class Storage:
     """Abstract base class for all storage implementations. All other methods
     should be privatised to keep the interface clean."""
-    
-    fileName = op.join(os.environ['HOME'], '.jpa2', 'entries')
     
     def getEntriesList(self, entryYear, entryMonth):
         """Get list of Id's for entries in year and month."""
@@ -105,7 +83,9 @@ class Storage:
 
 class SQLiteStorage(Storage):
     """Base for storages with SQLite (1 and 2) backends"""
-
+    
+    fileName = op.join(op.expanduser('~'), '.jpa2', 'entries')
+    
     def getEntriesList(self, entryYear, entryMonth):
         self.cur.execute('select eid from entries where year = %d and month = %d')
         rows = self.cur.fetchall()
@@ -117,69 +97,16 @@ class SQLite1Storage(SQLiteStorage):
     pass
 
 
+class SQLite2Storage(SQLiteStorage):
+    """Main storage with SQLite2 backend"""
+    pass
+
+
 class DBStorage(Storage):
     """Common storage class for all BerkeleyDB storages."""
     pass
 
 
-class ShelveStorage(DBStorage):
-    """Main storage with shelve backend. As shelve uses BerkeleyDB as its
-    backend, this class has its roots in Berkeley DB storage class."""
+class ShelveStorage(Storage):
+    """Main storage with shelve backend. Use as a last resort."""
     pass
-
-
-class MetakitStorage(Storage):
-    """Main storage of application data, based on metakit"""
-
-    def __init__(self):
-        import metakit
-        if op.isfile(self.fileName):
-            self.storage = metakit.storage(self.fileName, 1)
-            self.entries = self.storage.view('entries')
-        else:
-            self.__initStorage(self.fileName)
-
-    def __initStorage(self, fileName):
-        dirName = op.dirname(fileName)
-        if not op.isdir(dirName):
-            os.makedirs(dirName)
-        self.storage = metakit.storage(fileName, 1)
-        self.entries = self.storage.getas('entries[eid:S,created:I,sent:S,'
-            'edited:S,title:S,body:S,year:I,month:I]')
-    
-    def getEntriesList(self, entryYear, entryMonth):
-        view = self.entries.select(year=entryYear, month=entryMonth)
-        return view.project(metakit.property('S', 'eid'))
-    
-    def getEntries(self, entryYear, entryMonth):
-        return self.entries.select(year=entryYear, month=entryMonth)
-
-    def getEntry(self, entryId):
-        entry = {}
-        row = self.entries.find(eid=entryId)
-        if row:
-            entry['eid'] = row.eid
-            entry['created'] = pickle.loads(row.created)
-            entry['sent'] = pickle.loads(row.sent)
-            entry['edited'] = pickle.loads(row.edited)
-            entry['title'] = row.title
-            entry['body'] = row.body
-        return entry
-
-    def setEntry(self, entry, entryId=None):
-        if entryId:
-            row = self.entries.find(eid=entryId)
-            row.created = pickle.dumps(entry['created'], 2)
-            row.sent = pickle.dumps(entry['sent'], 2)
-            row.edited = pickle.dumps(entry['edited'], 2)
-            row.title = entry['title']
-            row.body = entry['body']
-        else:
-            entry['eid'] = time.strftime('%Y%m%d%H%M%S', entry['created'])
-            entry['year'] = entry['created'][0]
-            entry['month'] = entry['created'][1]
-            self.entries.append(**entry)
-
-    def close(self):
-        self.storage.commit()
-        self.storage = None
