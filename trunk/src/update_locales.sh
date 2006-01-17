da93bfd64b9e92a38a4a35f23c36a7c4fe6de4a9
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2006-01-17 20:07:01 +0000

    Updated Polish translation. Added missing folders to tool script.

--- trunk/src/update_locales.sh
+++ trunk/src/update_locales.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 intltool-extract --type=gettext/glade share/jpa2.glade
-xgettext -k_ -kN_ -o src/locale/messages.pot lib/*.py share/jpa2.glade.h
+xgettext -k_ -kN_ -o src/locale/messages.pot lib/*.py lib/transport/*.py lib/renderer/*.py share/jpa2.glade.h
 msgmerge -U src/locale/pl.po src/locale/messages.pot
-/usr/bin/vim src/locale/pl.po
+gedit src/locale/pl.po
 msgfmt src/locale/pl.po -o share/locale/pl/LC_MESSAGES/jpa.mo
 
