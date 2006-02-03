d5aa47d825d30c014e2173970dc0c829ed74aa24
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2006-02-03 12:46:32 +0000

    Missing module header.

--- trunk/lib/transport/proxytools.py
+++ trunk/lib/transport/proxytools.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# This file is part of JPA.
+# Copyright: (C) 2003 - 2005 Jarek Zgoda <jzgoda@o2.pl>
+#
+# JPA is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 2 of the License.
+#
+# JPA is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# JPA; if not, write to the Free Software Foundation, Inc., 
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+"""Proxy transport utilities"""
+
+__revision__ = '$Id$'
+
 # Taken from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/456195
 
-# urllib2 opener to connection through a proxy using the CONNECT method
+# urllib2 opener to allow connection through a proxy using the CONNECT method
 # tested with python 2.4
 
 import urllib
