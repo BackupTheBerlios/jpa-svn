db733a0ea725d7ed15107e0bf7ec49f3e2b30200
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2006-03-21 14:34:47 +0000

    Entry filter dialog looks much better.

--- trunk/lib/filter_dialog.py
+++ trunk/lib/filter_dialog.py
@@ -25,13 +25,17 @@ import datetime
 
 import gtk
 
+import datamodel
 from appwindow import EditDialog
 
 class FilterDialog(EditDialog):
     
-    def __init__(self, parent):
+    def __init__(self, parent, curFilter={}):
         EditDialog.__init__(self, 'dlgFilter', parent)
+        self.curFilter = curFilter
         self.cbxMonth = self.wTree.get_widget('cbxMonth')
+        self.lvCategories = self.wTree.get_widget('lvCategories')
+        self.ckbShowAllCategories = self.wTree.get_widget('ckbShowAllCategories')
         self._initGui()
     
     def _initGui(self):
@@ -46,7 +50,29 @@ class FilterDialog(EditDialog):
         self.cbxMonth.pack_start(cell, True)
         self.cbxMonth.add_attribute(cell, 'text', 0)
         self.cbxMonth.set_active(0)
+        catStore = gtk.ListStore(bool, str)
+        categories = datamodel.Category.select(orderBy='name')
+        for category in categories:
+            catStore.append((False, category.name))
+        cell0 = gtk.CellRendererToggle()
+        cell0.set_property('radio', False)
+        cell0.set_property('activatable', True)
+        cell0.connect('toggled', self.on_lvCategories_toggle, catStore)
+        column0 = gtk.TreeViewColumn('use', cell0, active=0)
+        cell1 = gtk.CellRendererText()
+        column1 = gtk.TreeViewColumn('name', cell1, text=1)
+        self.lvCategories.append_column(column0)
+        self.lvCategories.append_column(column1)
+        self.lvCategories.set_model(catStore)        
     
     def run(self):
         ret = self.window.run()
         self.window.destroy()
+    
+    ### signal handlers ###
+    def on_lvCategories_toggle(self, cell, path, model=None):
+        iter = model.get_iter(path)
+        model.set_value(iter, 0, not cell.get_active())
+
+    def on_ckbShowAllCategories_toggled(self, *args):
+        self.lvCategories.set_sensitive(not self.ckbShowAllCategories.get_active())
