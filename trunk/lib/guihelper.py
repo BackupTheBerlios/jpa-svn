f81ad0b58aa3b67d4ecffb21ca49c3b1afe943e0
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2007-01-01 13:19:52 +0000

    Work on guihelper continues.

--- trunk/lib/guihelper.py
+++ trunk/lib/guihelper.py
@@ -21,12 +21,75 @@
 __revision__ = '$Id$'
 
 
+import os
+import gettext
+
+try:
+    from xml.etree import ElementTree
+except ImportError:
+    try:
+        import cElementTree as ElementTree
+    except ImportError:
+        try:
+            import lxml.etree as ElementTree
+        except ImportError:
+            # if this one fails, we will be unable to go any further
+            from elementtree import ElementTree
+
+import gtk, gtk.glade
+
+
+def get_widget_names(glade_path, glade_file_name):
+    """Get list of all widgets in glade file."""
+    glade_file_path = os.path.join(glade_path, window_name)
+    tree = ElementTree.parse(glade_file_path)
+    root = tree.getroot()
+    widgets = root.findall('.//widget')
+    widget_names = []
+    for child_widget in widgets:
+        widget_names.append(child_widget.get('id'))
+    return widget_names
+
+
 class GtkGuiHelper:
 
-    def __init__(self, glade_path, app_name, path_conf):
+    def __init__(self, glade_path, app_name, i18n_path):
+        """Helper class constructor.
+        Expected arguments are: path to directory with glade files,
+        application name (used as gettext translation "domain" name) and
+        path to directory where translation catalogs reside."""
         self.glade_path = glade_path
         self.app_name = app_name
-        self.path_conf = path_conf
+        self.i18n_path = i18n_path
+        gtk.glade.bindtextdomain(app_name, i18n_path)
+        gtk.glade.textdomain(app_name)
+        gettext.install(app_name, i18n_path, unicode=True)
+        self.widget_trees = {}
 
-    def get_window(self, window_name):
-        pass
+    def _get_widget_tree(window_name):
+        try:
+            widget_tree = self.widget_trees[window_name]
+        except KeyError:
+            # this window is created for 1st time
+            glade_file_name = os.path.join(self.glade_path, window_name)
+            widget_tree = gtk.glade.XML(glade_file_name, window_name, \
+                self.app_name)
+            self.widget_trees[window_name] = widget_tree
+        return widget_tree
+
+    def get_widgets(self, window_name):
+        """This method returns tuple of toplevel widget (usually a window)
+        and a dictionary of child widgets (name => widget)."""
+        widget_tree = self._get_widget_tree(window_name)
+        window = widget_tree.get_widget(window_name)
+        widget_names = get_widget_names(self.glade_path, window_name)
+        widgets = {}
+        for widget_name in widget_names:
+            widgets[widget_name] = widget_tree.get_widget(widget_name)
+        return (window, widgets)
+
+    def connect_signals(self, window_name, signal_dict):
+        """This method connect signals defined in glade file to dictionary
+        of signals, usually some class instance."""
+        widget_tree = self._get_widget_tree(window_name)
+        widget_tree.signal_autoconnect(signal_dict)
