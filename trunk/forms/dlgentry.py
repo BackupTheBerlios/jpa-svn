96cb0534781072ed8b334f2e5c65b4f7f9972267
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2007-03-23 22:22:37 +0000

    Entry dialog editor.

---
+++ trunk/forms/dlgentry.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2003-2007 Jarek Zgoda <jzgoda@o2.pl>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Entry editing dialog"""
+
+__revision__ = '$Id$'
