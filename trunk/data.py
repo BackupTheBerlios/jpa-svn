72b49159ccc9f0419c02f8f0940faf9a9de580b4
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2007-04-04 21:00:34 +0000

    Datamodel preliminary work.

--- trunk/data.py
+++ trunk/data.py
@@ -9,3 +9,34 @@
 """Application data model and support routines"""
 
 __revision__ = '$Id$'
+
+import os
+import datetime
+
+from sqlalchemy import *
+
+import const
+
+class Data(object):
+
+    def __init__(self):
+        db_filename = os.path.abspath(os.path.join(const.USER_DIR, 'jpa.db'))
+        self.metadata = BoundMetadata('sqlite://%s' % db_filename)
+        self.metadata.engine.echo = False
+        self.weblogs_table = Table('weblog', metadata,
+            Column('weblog_id', Integer, primary_key=True),
+            Column('name', Unicode(80)),
+            Column('description', Unicode),
+        )
+        self.entries_table = Table('entry', metadata,
+            Column('entry_id', Integer, primary_key=True),
+            Column('created', DateTime, default=datetime.datetime.now()),
+            Column('title', Unicode(100), nullable=False),
+            Column('text', Unicode),
+            Column('content_type', Unicode(20), default=u'markdown'),
+            Column('labels', Unicode),
+        )
+
+    def init_tables():
+        weblogs_table.create()
+        entries_table.create()
