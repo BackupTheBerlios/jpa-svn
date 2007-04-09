7ac71aee1f4c39772fe95a6adea6583475e154cb
zgoda <zgoda@45e96cff-cfd4-0310-b64b-b8e4ff359551>
2007-04-09 19:55:42 +0000

    Preliminary work on saving configuration data.

--- trunk/forms/dlgprefs.py
+++ trunk/forms/dlgprefs.py
@@ -10,6 +10,9 @@
 
 __revision__ = '$Id$'
 
+import os
+from ConfigParser import NoSectionError, NoOptionError
+
 import gtk
 import gtk.glade
 
@@ -29,22 +32,64 @@ class PreferencesWindow(object):
         self.w_tree.signal_autoconnect(self)
         self.window = self.w_tree.get_widget('dlg_prefs')
         forms.set_window_icon(self.window)
+        self.cbx_text_type = self.w_tree.get_widget('cbx_text_type')
+        self.check_save_auth = self.w_tree.get_widget('check_save_auth')
         self.ed_login = self.w_tree.get_widget('ed_login')
+        self.ed_password = self.w_tree.get_widget('ed_password')
         self.tbl_auth_data = self.w_tree.get_widget('tbl_auth_data')
+        self._load_config()
         self.run()
 
     def run(self):
         while 1:
             ret = self.window.run()
             if ret == gtk.RESPONSE_APPLY:
-                pass
+                self.save_config()
             elif ret == gtk.RESPONSE_OK:
+                self.save_config()
                 break
             else:
                 break
         self.window.destroy()
 
+    def _load_config(self):
+        try:
+            auth = dict(self.cfg.items('auth'))
+        except NoSectionError:
+            auth = {}
+        save_auth = (auth.get('save_auth', '1') == '1')
+        self.ed_login.set_text(auth.get('login', ''))
+        self.ed_password.set_text(auth.get('password', ''))
+        self.check_save_auth.set_active(save_auth)
+
     # GTK signal handlers
     def on_check_save_auth_toggled(self, *args):
         self.tbl_auth_data.set_sensitive(args[0].get_active())
         self.ed_login.grab_focus()
+
+    # miscellaneous
+    def save_config(self):
+        # save configuration by sections
+        # Blogger authorization data
+        if not self.cfg.has_section('auth'):
+            self.cfg.add_section('auth')
+        save_auth = self.check_save_auth.get_active()
+        if save_auth:
+            login = self.ed_login.get_text()
+            password = self.ed_password.get_text()
+        else:
+            login = ''
+            password = ''
+        self.cfg.set('auth', 'login', login)
+        self.cfg.set('auth', 'password', password)
+        if save_auth:
+            value = '1'
+        else:
+            value = '0'
+        self.cfg.set('auth', 'save_auth', value)
+        config_file = os.path.join(const.USER_DIR, 'config')
+        fp = open(config_file, 'w')
+        try:
+            self.cfg.write(fp)
+        finally:
+            fp.close()
